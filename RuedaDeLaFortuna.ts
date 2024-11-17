3bd52db0b9c5545735d3467858dfec634985a44e
Mauricio <claum_55@hotmail.com>
2024-11-17 13:28:43 -0300

    se sube rama con logica de rueda fortuna

---
+++ RuedaDeLaFortuna.ts
@@ -0,0 +1,61 @@
+import * as rls from "readline-sync";
+
+export class RuedaDeLaFortuna {
+private premios: string[] = [
+    "¡Perdiste!",
+    "¡Perdiste!",
+    "¡Perdiste!",
+    "¡Perdiste!",
+    "¡Perdiste!",
+    "¡Perdiste!",
+    "100",
+    "200",
+    "500",
+    "1000",
+];
+
+constructor() {
+    this.mostrarPremios();
+    this.jugar();
+}
+
+private mostrarPremios(): void {
+    console.log(" Bienvenido a la Rueda de la Fortuna ");
+    console.log("Premios posibles:");
+    this.premios.forEach((premio, index) => {
+    console.log(`${index + 1}. ${premio}`);
+    });
+}
+
+private jugar(): void {
+    let deseaContinuar = true;
+    while (deseaContinuar) {
+        //Aqui va la apuesta
+    const deseaJugar = rls
+        .question("¿Quieres girar la rueda? (s/n): ")
+        .toLowerCase();
+    if (deseaJugar === "s") {
+            console.log("\n Girando la rueda... ");
+            const resultado = this.obtenerResultado();
+            console.log(` Resultado: ${resultado}`);
+            if (resultado.includes("Perdiste")) {
+            console.log(" Mejor suerte la próxima vez.");
+            } else {
+            console.log(`¡Felicitaciones! Ganaste ${resultado} creditos.`);
+            };
+            const jugarNuevamente = rls.question("¿Quieres jugar otra vez? (s/n): ").toLowerCase();
+            deseaContinuar = jugarNuevamente === "s";
+            } else {
+            deseaContinuar = false;
+        }
+    }
+    console.log("Gracias por jugar. ¡Hasta la próxima!");
+}
+
+private obtenerResultado(): string {
+    const indice = Math.floor(Math.random() * this.premios.length);
+    return this.premios[indice];
+    }
+}
+
+new RuedaDeLaFortuna();
