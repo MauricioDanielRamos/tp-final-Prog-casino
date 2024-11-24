8085f20cfed13dcf50818f756ca3f6badb65c3b6
Mauricio <claum_55@hotmail.com>
2024-11-24 17:06:38 -0300

    Se corrije IJuego y Class Juego

--- Juego.ts
+++ Juego.ts
@@ -1,6 +1,7 @@
 import { Usuario } from "./Usuario";
+import { IJuego } from "./IJuego";
 
-export abstract class Juego {
+export abstract class Juego implements IJuego {
 	//nombre del juego
 	private nombre: string;
 
@@ -15,7 +16,7 @@ export abstract class Juego {
 	}
 
 	/* Declara un método abstracto llamado "jugar" que debe ser implementado en las clases hijas.
-	 Este método recibe un parámetro de tipo "Sesion" y no tiene implementación en la clase base.
+	Este método recibe un parámetro de tipo "Usuario" y no tiene implementación en la clase base.
 	Las clases que hereden de Juego deberán definir cómo se lleva a cabo el juego utilizando este método.*/
 	public abstract jugar(usuario: Usuario): void;
 }
