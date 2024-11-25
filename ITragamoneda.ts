6f346754555a8f342f27535c8a5a114920618d59
Aldo Lopez <aldo.lopez@gmail.com>
2024-11-25 23:30:04 +0000

    Revert "agrego rueda de la fortuna y se agrega juego al main"
    This reverts commit 81d1fc6bc098c38312249731687d3d6243fef943.

--- ITragamoneda.ts
+++
@@ -1,6 +0,0 @@
-import { Usuario } from "./Usuario";
-
-export interface ITragamoneda {
-    nombreJuego: string; // Nombre del juego
-    jugar(usuario: Usuario): void; // Método principal para jugar
-}
