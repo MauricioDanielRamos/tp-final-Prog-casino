a860e7ca191b49deb9f828574650cb405e4f81f8
Aldo Lopez <aldo.lopez@gmail.com>
2024-11-28 12:52:57 +0000

    Corrección ya implementada en main (Para no hacer un pull request nuevamente)

--- Util.ts
+++ Util.ts
@@ -13,7 +13,7 @@ export class Util {
             if (resultado.length>0){
                 //Si se pasaron reemplazos de texto
                 if (reemplazos!==undefined){
-                    reemplazos.forEach((reemplazo)=>resultado = resultado.replace(reemplazo.clave, reemplazo.valor))
+                    reemplazos.forEach((reemplazo)=>resultado = resultado.replace(`/${reemplazo.clave}/g`, reemplazo.valor))
                 }            
             }
         } catch (error) {
