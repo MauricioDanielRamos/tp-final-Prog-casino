8a8d8a519f327dae972d54d807c5cc2d9f3b8fe0
Aldo Lopez <aldo.lopez@gmail.com>
2024-11-29 22:12:59 +0000

    Corregida definicion de IJuego e implementación de Juego

--- Juego.ts
+++ Juego.ts
@@ -2,7 +2,7 @@ import { Usuario } from "./Usuario";
 import { IJuego } from "./IJuego";
 import { ParValorClave, Util } from "./Util";
 
-export abstract class Juego {
+export abstract class Juego implements IJuego{
 	//nombre del juego
 	private nombre: string;
 
@@ -24,7 +24,7 @@ export abstract class Juego {
 	}
 
 	/* Declara un método abstracto llamado "jugar" que debe ser implementado en las clases hijas.
-	 Este método recibe un parámetro de tipo "Sesion" y no tiene implementación en la clase base.
+	 Este método recibe un parámetro de tipo "Usuario" y no tiene implementación en la clase base.
 	Las clases que hereden de Juego deberán definir cómo se lleva a cabo el juego utilizando este método.*/
 	public abstract jugar(usuario: Usuario): void;
 }
