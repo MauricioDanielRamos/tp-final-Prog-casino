9cb9dedf9d7f604f46a5c6400c92723cecd58cec
Aldo Lopez <aldo.lopez@gmail.com>
2024-12-06 23:53:27 +0000

    Optimización código RuedaDeLaFortuna

--- RuedaDeLaFortuna.ts
+++ RuedaDeLaFortuna.ts
@@ -48,150 +48,104 @@ export class RuedaDeLaFortuna extends Juego {
 
 	// Función principal para iniciar el juego
 	public jugar(usuario: Usuario): void {
-		this.mostrarInstrucciones([
-			{ clave: "$<NOMBRE_USUARIO>", valor: usuario.getNombre() },
-			{
-				clave: "$<CREDITOS>",
-				valor: Util.convertirAPesosAR(usuario.getCreditos()),
-			},
-			{ clave: "$<NOMBRE_JUEGO>", valor: this.getNombre() },
-		]);
-		// Verifica si el usurio está definido, si no lo está, muestra un error
-		if (!usuario) {
-			console.log("Error: Usuario no definido.");
-			// Pausa para que el usuario lea el mensaje antes de continuar
-			rls.keyInPause("Presione una tecla para continuar", {
-				guide: false,
-			});
-		}
+		let salir = false;
+		while (!salir){
+			console.clear();
+			this.mostrarInstrucciones([
+				{ clave: "$<NOMBRE_USUARIO>", valor: usuario.getNombre() },
+				{
+					clave: "$<CREDITOS>",
+					valor: Util.convertirAPesosAR(usuario.getCreditos()),
+				},
+				{ clave: "$<NOMBRE_JUEGO>", valor: this.getNombre() },
+				{ clave: "$<TIRADA>", valor: Util.convertirAPesosAR(CREDITOS_MINIMOS) },
+			]);
+			// Verifica si el usurio está definido, si no lo está, muestra un error
+			if (!usuario) {
+				console.log("Error: Usuario no definido.");
+				// Pausa para que el usuario lea el mensaje antes de continuar
+				rls.keyInPause("Presione una tecla para continuar", {
+					guide: false,
+				});
+				salir=true;
+			}
 
-		// Verifica si el usuario tiene suficientos creditos pira jugar
-		if (usuario.getCreditos() < CREDITOS_MINIMOS) {
-			//Muestra los creditos actuales
-			this.mostrarCreditos(usuario);
-			console.log(
-				`No tienes suficientes creditos para jugar. Los creditos minimos para este juego son: ${Util.convertirAPesosAR(
-					CREDITOS_MINIMOS
-				)} `
+			// Verifica si el usuario tiene suficientos creditos para jugar
+			if (usuario.getCreditos() < CREDITOS_MINIMOS) {
+				//Muestra los creditos actuales
+				this.mostrarCreditos(usuario);
+				console.log(
+					`No tienes suficientes creditos para jugar. Los creditos minimos para este juego son: ${Util.convertirAPesosAR(
+						CREDITOS_MINIMOS
+					)} `
+				);
+				// Pausa para que el usuario lea el mensaje antes de continuar
+				rls.keyInPause("Presione una tecla para continuar", {
+					guide: false,
+				});
+				// Termina la ejecución de la función si los creditos sin insuficientes
+				salir=true;
+			}
+
+			// Muestra los premios posibles
+			console.log("Premios posibles:");
+			this.premios.forEach((premio, index) =>
+				console.log(`${index + 1}. ${premio}`)
 			);
-			// Pausa para que el usuario lea el mensaje antes de continuar
-			rls.keyInPause("Presione una tecla para continuar", {
-				guide: false,
-			});
-			// Termina la ejecución de la función si los creditos sin insuficientes
-			return;
+
+			// Llama a la función para realizar la tirada de la ruleta
+			if (!salir){
+				const opciones = ["Si", "No"];
+				// Pregunta al usuario si desea hacer la tirada
+				const eleccion: number = rls.keyInSelect(opciones, "¿Desea girar la rueda? ", {
+					guide: false,
+					cancel: false,
+				});
+				switch (eleccion) {
+					case 0: this.realizarTirada(usuario);
+							break;
+					case 1: salir=true;
+							break;
+				}
+			}
 		}
-
-		// Muestra los premios posibles
-		console.log("Premios posibles:");
-		this.premios.forEach((premio, index) =>
-			console.log(`${index + 1}. ${premio}`)
-		);
-
-		// Llama a la función para realizar la tirada de la ruleta
-		this.realizarTirada(usuario);
+		// Si el usuario elige "No", termina el juego
+		console.log("Gracias por jugar. ¡Hasta la proxima!");
+		// Pausa antes de finalizar
+		rls.keyInPause("Presione una tecla para continuar", {
+			guide: false,
+		});
 	}
 
 	// Funcion para manejar la lógica de la tirada del juego
 	private realizarTirada(usuario: Usuario): void {
-		// Verifica si el usuario tiene suficientes creditos pira realizar una tirada
-		if (usuario.getCreditos() < CREDITOS_MINIMOS) {
-			console.log("No tienes mas creditos pira realizar una tirada.");
-			// Pausa para que el usuario lea el mensaje antes de continuar
-			rls.keyInPause("Presione una tecla para continuar", {
-				guide: false,
-			});
-			// Termina la ejecución de la función si no tiene los creditos sificientes
-			return;
-		}
+		// Si el usuario tiene créditos suficientes, se le descuentan los creditos por la tirada
+		usuario.setCreditos(-CREDITOS_MINIMOS);
+		// Limpia la consola para mostrar el resultado
+		console.clear();
+		console.log("Girando la rueda...");
 
-		// Muestra el costo de la tirada
-		console.log(`Tirada: $1000`);
-		// Muestra los creditos aituales antes de hacer la tirada
-		this.mostrarCreditos(usuario);
-		// Opciones que el usuario puede seleccionar (Sí o No)
-		const opciones = ["Si", "No"];
-		// Pregunta al usuario si desea hacer la tirada
-		const eleccion = rls.keyInSelect(opciones, "¿Desea hacer un tiro? ", {
-			guide: false,
-			cancel: false,
-		});
+		// Obtiene el resultado de la rueda
+		const resultado = this.obtenerResultado();
+		console.log(`Resultado: ${resultado}`);
 
-		// Si el usuario elige "Sí" (elección 0)
-		if (eleccion === 0) {
-			// Verifica nuevamente si el usuario tiene los creditos sificientes antes de continuar
-			if (usuario.getCreditos() < CREDITOS_MINIMOS) {
-				console.log(
-					`Error: Creditos insuficientes para realizar la tirada. Tienes ${usuario.getCreditos()} Creditos`
-			);
-				// Pausa antes de finalizar
-				rls.keyInPause("Presione una tecla para continuar", {
-					guide: false,
-				});
-				return;
-			} else {
-				// Si el usuario tiene creditos sificientes, se le descuentan los creditos pir la tirada
-				usuario.setCreditos(-CREDITOS_MINIMOS);
-				// Limpia la consola para mostrar el resultado
-				console.clear();
-				console.log("Girando la rueda...");
-
-				// Obtiene el resultado de la rueda
-				const resultado = this.obtenerResultado();
-				console.log(`Resultado: ${resultado}`);
-
-				// Si el resultado es "¡Perdiste!", muestra un mensaje
-				if (resultado === "¡Perdiste!") {
-					console.log("Mejor suerte la proxima vez.");
-				} else if (!isNaN(parseFloat(resultado))) {
-					// Si el resultado es un número, se suman los creditos oitenidos
-					const premio = parseFloat(resultado);
-					usuario.setCreditos(premio);
-					console.log(
-						`¡Felicitaciones! Ganaste ${Util.convertirAPesosAR(
-							premio
-						)} en creditos.`);
-				} else {
-					// Si el resultado es un premio en texto, muestra el premio
-					console.log(`¡Felicitaciones! Ganaste: ${resultado}`);
-				}
-
-				// Muestra los creditos aituales del usuario despues de la tirada
-				this.mostrarCreditos(usuario);
-
-				// Verificar si el usuario tiene suficientes creditos pira seguir jugando
-				if (usuario.getCreditos() < CREDITOS_MINIMOS) {
-					console.log(
-						"No tienes suficientes creditos pira realizar otra tirada."
-					);
-					// Pausa antes de finalizar
-					rls.keyInPause("Presione una tecla para continuar", {
-						guide: false,
-					});
-					return;
-				}
-
-				// Preguntar si desea seguir jugando
-				const continuar = rls.keyInSelect(
-					opciones,
-					"¿Desea seguir jugando?",
-					{ cancel: false, guide: false }
-				);
-				// Si el usuario elige "Si", se realiza otra tirada
-				if (continuar === 0) {
-					this.realizarTirada(usuario); // Llamada recursiva para otra tirada
-				} else {
-					// Si el usuario elige "No", termina el juego
-					console.log("Gracias por jugar. ¡Hasta la proxima!");
-					// Pausa antes de finalizar
-					rls.keyInPause("Presione una tecla para continuar", {
-						guide: false,
-					});
-				}
-			}
+		// Si el resultado es "¡Perdiste!", muestra un mensaje
+		if (resultado === "¡Perdiste!") {
+			console.log("Mejor suerte la proxima vez.");
+		} else if (!isNaN(parseFloat(resultado))) {
+			// Si el resultado es un número, se suman los creditos obtenidos
+			const premio = parseFloat(resultado);
+			usuario.setCreditos(premio);
+			console.log(
+				`¡Felicitaciones! Ganaste ${Util.convertirAPesosAR(
+					premio
+				)} en creditos.`);
 		} else {
-			// Si el usuario elige "No", termina el juego
-			console.log("Gracias por jugar. ¡Hasta la proxima!");
+			// Si el resultado es un premio en texto, muestra el premio
+			console.log(`¡Felicitaciones! Ganaste: ${resultado}`);
 		}
+		rls.keyInPause("Presione una tecla para continuar", {
+			guide: false,
+		});
 	}
 }
